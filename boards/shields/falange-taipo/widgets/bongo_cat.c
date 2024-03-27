1d4473f98ceafa50fd879f08a352b36d751373e3
Pedro Casotti <pedro.c.casotti@gmail.com>
2024-03-27 00:43:38 -0300

    Taipo?

---
+++ boards/shields/falange-taipo/widgets/bongo_cat.c
@@ -0,0 +1,159 @@
+/*
+ * Copyright (c) 2024 The ZMK Contributors
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#include <zephyr/kernel.h>
+#include <zephyr/bluetooth/services/bas.h>
+
+#include <zephyr/logging/log.h>
+LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
+
+#include <zmk/display.h>
+#include <zmk/event_manager.h>
+#include <zmk/events/wpm_state_changed.h>
+#include <zmk/wpm.h>
+
+#include "bongo_cat.h"
+
+#define SRC(array) (const void **)array, sizeof(array) / sizeof(lv_img_dsc_t *)
+
+static sys_slist_t widgets = SYS_SLIST_STATIC_INIT(&widgets);
+
+LV_IMG_DECLARE(bongo_cat_none);
+LV_IMG_DECLARE(bongo_cat_left1);
+LV_IMG_DECLARE(bongo_cat_left2);
+LV_IMG_DECLARE(bongo_cat_right1);
+LV_IMG_DECLARE(bongo_cat_right2);
+LV_IMG_DECLARE(bongo_cat_both1);
+LV_IMG_DECLARE(bongo_cat_both1_open);
+LV_IMG_DECLARE(bongo_cat_both2);
+
+#define ANIMATION_SPEED_IDLE 10000
+const lv_img_dsc_t *idle_imgs[] = {
+    &bongo_cat_both1_open,
+    &bongo_cat_both1_open,
+    &bongo_cat_both1_open,
+    &bongo_cat_both1,
+};
+
+#define ANIMATION_SPEED_SLOW 2000
+const lv_img_dsc_t *slow_imgs[] = {
+    &bongo_cat_left1,
+    &bongo_cat_both1,
+    &bongo_cat_both1,
+    &bongo_cat_right1,
+    &bongo_cat_both1,
+    &bongo_cat_both1,
+    &bongo_cat_left1,
+    &bongo_cat_both1,
+    &bongo_cat_both1,
+};
+
+#define ANIMATION_SPEED_MID 500
+const lv_img_dsc_t *mid_imgs[] = {
+    &bongo_cat_left2,
+    &bongo_cat_left1,
+    &bongo_cat_none,
+    &bongo_cat_right2,
+    &bongo_cat_right1,
+    &bongo_cat_none,
+};
+
+#define ANIMATION_SPEED_FAST 200
+const lv_img_dsc_t *fast_imgs[] = {
+    &bongo_cat_both2,
+    &bongo_cat_both1,
+    &bongo_cat_none,
+    &bongo_cat_none,
+};
+
+struct bongo_cat_wpm_status_state {
+    uint8_t wpm;
+};
+
+enum anim_state {
+    anim_state_none,
+    anim_state_idle,
+    anim_state_slow,
+    anim_state_mid,
+    anim_state_fast
+} current_anim_state;
+
+lv_obj_t *label;
+lv_obj_t *anim;
+
+static void set_animation(lv_obj_t *widget, struct bongo_cat_wpm_status_state state) {
+    if (state.wpm < 5) {
+        if (current_anim_state != anim_state_idle) {
+            lv_animimg_set_src(anim, SRC(idle_imgs));
+            lv_animimg_set_duration(anim, ANIMATION_SPEED_IDLE);
+            lv_animimg_set_repeat_count(anim, LV_ANIM_REPEAT_INFINITE);
+            lv_animimg_start(anim);
+            current_anim_state = anim_state_idle;
+        }
+    } else if (state.wpm < 30) {
+        if (current_anim_state != anim_state_slow) {
+            lv_animimg_set_src(anim, SRC(slow_imgs));
+            lv_animimg_set_duration(anim, ANIMATION_SPEED_SLOW);
+            lv_animimg_set_repeat_count(anim, LV_ANIM_REPEAT_INFINITE);
+            lv_animimg_start(anim);
+            current_anim_state = anim_state_slow;
+        }
+    } else if (state.wpm < 70) {
+        if (current_anim_state != anim_state_mid) {
+            lv_animimg_set_src(anim, SRC(mid_imgs));
+            lv_animimg_set_duration(anim, ANIMATION_SPEED_MID);
+            lv_animimg_set_repeat_count(anim, LV_ANIM_REPEAT_INFINITE);
+            lv_animimg_start(anim);
+            current_anim_state = anim_state_mid;
+        }
+    } else {
+        if (current_anim_state != anim_state_fast) {
+            lv_animimg_set_src(anim, SRC(fast_imgs));
+            lv_animimg_set_duration(anim, ANIMATION_SPEED_FAST);
+            lv_animimg_set_repeat_count(anim, LV_ANIM_REPEAT_INFINITE);
+            lv_animimg_start(anim);
+            current_anim_state = anim_state_fast;
+        }
+    }
+
+    lv_label_set_text_fmt(label, "%i", state.wpm);
+}
+
+struct bongo_cat_wpm_status_state bongo_cat_wpm_status_get_state(const zmk_event_t *eh) {
+    struct zmk_wpm_state_changed *ev = as_zmk_wpm_state_changed(eh);
+    return (struct bongo_cat_wpm_status_state) { .wpm = ev->state };
+};
+
+void bongo_cat_wpm_status_update_cb(struct bongo_cat_wpm_status_state state) {
+    struct zmk_widget_bongo_cat *widget;
+    SYS_SLIST_FOR_EACH_CONTAINER(&widgets, widget, node) { set_animation(widget->obj, state); }
+}
+
+ZMK_DISPLAY_WIDGET_LISTENER(widget_bongo_cat, struct bongo_cat_wpm_status_state,
+                            bongo_cat_wpm_status_update_cb, bongo_cat_wpm_status_get_state)
+
+ZMK_SUBSCRIPTION(widget_bongo_cat, zmk_wpm_state_changed);
+
+int zmk_widget_bongo_cat_init(struct zmk_widget_bongo_cat *widget, lv_obj_t *parent) {
+    widget->obj = lv_obj_create(parent);
+    lv_obj_set_size(widget->obj, 60, 31);
+
+    anim = lv_animimg_create(widget->obj);
+    lv_obj_align(anim, LV_ALIGN_TOP_RIGHT, 0, 0);
+
+    label = lv_label_create(widget->obj);
+    lv_obj_align(label, LV_ALIGN_BOTTOM_LEFT, 0, 0);
+
+    sys_slist_append(&widgets, &widget->node);
+
+    widget_bongo_cat_init();
+
+    return 0;
+}
+
+lv_obj_t *zmk_widget_bongo_cat_obj(struct zmk_widget_bongo_cat *widget) {
+    return widget->obj;
+}
---
+++ boards/shields/falange-taipo/widgets/modifiers_sym.c
@@ -0,0 +1,147 @@
+/*
+ * Copyright (c) 2024 The ZMK Contributors
+ *
+ * SPDX-License-Identifier: MIT
+ */
+ 
+ #include <lvgl.h>
+
+#ifndef LV_ATTRIBUTE_MEM_ALIGN
+#define LV_ATTRIBUTE_MEM_ALIGN
+#endif
+
+#ifndef LV_ATTRIBUTE_IMG_CONTROL
+#define LV_ATTRIBUTE_IMG_CONTROL
+#endif
+
+const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_CONTROL uint8_t control_map[] = {
+  0xff, 0xff, 0xff, 0xff, 	/*Color of index 0*/
+  0x00, 0x00, 0x00, 0xff, 	/*Color of index 1*/
+
+  0x00, 0x00,
+  0x07, 0x80,
+  0x38, 0x70,
+  0x38, 0x70,
+  0x3c, 0xf0,
+  0x4c, 0xc8,
+  0x43, 0x08,
+  0x64, 0x98,
+  0x5c, 0xe8,
+  0x23, 0x10,
+  0x20, 0x10,
+  0x18, 0x60,
+  0x07, 0x80,
+  0x00, 0x00,
+};
+
+const lv_img_dsc_t control_icon = {
+  .header.cf = LV_IMG_CF_INDEXED_1BIT,
+  .header.always_zero = 0,
+  .header.reserved = 0,
+  .header.w = 14,
+  .header.h = 14,
+  .data_size = 36,
+  .data = control_map,
+};
+
+#ifndef LV_ATTRIBUTE_IMG_SHIFT
+#define LV_ATTRIBUTE_IMG_SHIFT
+#endif
+
+const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_SHIFT uint8_t shift_map[] = {
+  0xff, 0xff, 0xff, 0xff, 	/*Color of index 0*/
+  0x00, 0x00, 0x00, 0xff, 	/*Color of index 1*/
+
+  0x00, 0x00,
+  0x07, 0x80,
+  0x18, 0x60,
+  0x24, 0x10,
+  0x28, 0x10,
+  0x40, 0x08,
+  0x43, 0x08,
+  0x64, 0x98,
+  0x5c, 0xe8,
+  0x23, 0x10,
+  0x20, 0x10,
+  0x18, 0x60,
+  0x07, 0x80,
+  0x00, 0x00,
+};
+
+const lv_img_dsc_t shift_icon = {
+  .header.cf = LV_IMG_CF_INDEXED_1BIT,
+  .header.always_zero = 0,
+  .header.reserved = 0,
+  .header.w = 14,
+  .header.h = 14,
+  .data_size = 36,
+  .data = shift_map,
+};
+
+#ifndef LV_ATTRIBUTE_IMG_ALT
+#define LV_ATTRIBUTE_IMG_ALT
+#endif
+
+const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_ALT uint8_t alt_map[] = {
+  0xff, 0xff, 0xff, 0xff, 	/*Color of index 0*/
+  0x00, 0x00, 0x00, 0xff, 	/*Color of index 1*/
+
+  0x00, 0x00,
+  0x07, 0x80,
+  0x1f, 0xe0,
+  0x38, 0x70,
+  0x38, 0x70,
+  0x58, 0x68,
+  0x5b, 0x68,
+  0x7c, 0xf8,
+  0x44, 0x88,
+  0x23, 0x10,
+  0x20, 0x10,
+  0x18, 0x60,
+  0x07, 0x80,
+  0x00, 0x00,
+};
+
+const lv_img_dsc_t alt_icon = {
+  .header.cf = LV_IMG_CF_INDEXED_1BIT,
+  .header.always_zero = 0,
+  .header.reserved = 0,
+  .header.w = 14,
+  .header.h = 14,
+  .data_size = 36,
+  .data = alt_map,
+};
+
+#ifndef LV_ATTRIBUTE_IMG_GUI
+#define LV_ATTRIBUTE_IMG_GUI
+#endif
+
+const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_GUI uint8_t gui_map[] = {
+  0xff, 0xff, 0xff, 0xff, 	/*Color of index 0*/
+  0x00, 0x00, 0x00, 0xff, 	/*Color of index 1*/
+
+  0x00, 0x00,
+  0x07, 0x80,
+  0x38, 0x70,
+  0x38, 0x70,
+  0x38, 0x70,
+  0x70, 0x38,
+  0x43, 0x08,
+  0x64, 0x98,
+  0x5c, 0xe8,
+  0x23, 0x10,
+  0x20, 0x10,
+  0x18, 0x60,
+  0x07, 0x80,
+  0x00, 0x00,
+};
+
+const lv_img_dsc_t gui_icon = {
+  .header.cf = LV_IMG_CF_INDEXED_1BIT,
+  .header.always_zero = 0,
+  .header.reserved = 0,
+  .header.w = 14,
+  .header.h = 14,
+  .data_size = 36,
+  .data = gui_map,
+};
