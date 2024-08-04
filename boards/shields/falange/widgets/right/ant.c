de7035a1b542fce38c5f0655956fc027f80837ca
Pedro Casotti <pedro.c.casotti@gmail.com>
2024-08-03 21:34:32 -0300

    Back to sand

---
+++ boards/shields/falange/widgets/right/ant.c
@@ -0,0 +1,135 @@
+/*
+ * Copyright (c) 2024 The ZMK Contributors
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#include <stdint.h>
+#include <zephyr/kernel.h>
+
+#include <zephyr/logging/log.h>
+LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
+
+#include <zmk/display.h>
+#include <zmk/event_manager.h>
+#include <zmk/events/position_state_changed.h>
+
+#include "key_press.h"
+
+static sys_slist_t widgets = SYS_SLIST_STATIC_INIT(&widgets);
+
+struct key_press_state {
+    uint32_t position;
+    bool state;
+};
+
+static lv_color_t canvas_buffer[50 * 32];
+
+static uint8_t x = 25;
+static uint8_t y = 16;
+static uint8_t dir = 0;
+
+static uint8_t prev_x[200] = {0};
+static uint8_t prev_y[200] = {0};
+static uint8_t head = 0;
+
+static lv_anim_t a;
+
+void anim_reset(lv_obj_t *canvas) {
+    x = 15;
+    y = 15;
+    dir = 0;
+    canvas_reset(canvas);
+}
+
+void canvas_reset(lv_obj_t *canvas) {
+    lv_draw_rect_dsc_t rect_dsc;
+    lv_draw_rect_dsc_init(&rect_dsc);
+    rect_dsc.bg_color = lv_color_white();
+
+    lv_canvas_set_buffer(canvas, canvas_buffer, 50, 32, LV_IMG_CF_TRUE_COLOR);
+    lv_canvas_fill_bg(canvas, lv_color_black(), LV_OPA_COVER);
+    lv_canvas_draw_rect(canvas, 1, 1, 48, 30, &rect_dsc);
+}
+
+void anim_exec(void *canvas, int32_t val) {
+    // if (x == 0 || x == 49 || y == 0 || y == 31) anim_reset(canvas);
+
+    if (lv_canvas_get_px(canvas, x, y).full == lv_color_black().full) {
+        dir++;
+        lv_canvas_set_px(canvas, x, y, lv_color_white());
+    } else {
+        dir--;
+        lv_canvas_set_px(canvas, x, y, lv_color_black());
+
+        prev_x[head] = x;
+        prev_y[head] = y;
+        head++;
+        head %= 200;
+    }
+    dir %= 8;
+
+    if (dir == 0 || dir == 1) y++;
+    else if (dir == 2 || dir == 3) x++;
+    else if (dir == 4 || dir == 5) y--;
+    else if (dir == 6 || dir == 7) x--;
+
+    if (x == 0) x = 48;
+    if (x == 49) x = 1;
+    if (y == 0) y = 30;
+    if (y == 31) y = 1;
+
+    lv_canvas_set_px(canvas, prev_x[head], prev_y[head], lv_color_white());
+}
+
+static void set_symbol(lv_obj_t *widget, struct key_press_state state) {
+    lv_anim_del_all();
+    lv_anim_start(&a);
+}
+
+void key_press_update_cb(struct key_press_state state) {
+    struct zmk_widget_key_press *widget;
+    SYS_SLIST_FOR_EACH_CONTAINER(&widgets, widget, node) { set_symbol(widget->obj, state); }
+}
+
+static struct key_press_state key_press_get_state(const zmk_event_t *eh) {
+    const struct zmk_position_state_changed *ev = as_zmk_position_state_changed(eh);
+    return (struct key_press_state){
+        .position = ev->position,
+        .state = ev->state,
+    };
+}
+
+ZMK_DISPLAY_WIDGET_LISTENER(widget_key_press, struct key_press_state,
+                            key_press_update_cb, key_press_get_state)
+
+ZMK_SUBSCRIPTION(widget_key_press, zmk_position_state_changed);
+
+int zmk_widget_key_press_init(struct zmk_widget_key_press *widget, lv_obj_t *parent) {
+    widget->obj = lv_canvas_create(parent);
+
+    lv_obj_align(widget->obj, LV_ALIGN_BOTTOM_LEFT, 0, 0);
+
+    canvas_reset(widget->obj);
+
+    lv_anim_del_all();
+    lv_anim_init(&a);
+    lv_anim_set_exec_cb(&a, (lv_anim_exec_xcb_t) anim_exec);
+    lv_anim_set_var(&a, widget->obj);
+    lv_anim_set_time(&a, 30000);
+    lv_anim_set_values(&a, 1, 600);
+
+    // lv_anim_set_repeat_count(&a, LV_ANIM_REPEAT_INFINITE);
+
+    lv_anim_start(&a);
+
+    sys_slist_append(&widgets, &widget->node);
+
+    widget_key_press_init();
+
+    return 0;
+}
+
+lv_obj_t *zmk_widget_key_press_obj(struct zmk_widget_key_press *widget) {
+    return widget->obj;
+}
